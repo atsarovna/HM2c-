3daecd879410aa95438cf5a30208e50788133f49
Jane <evgenya.mun@yandex.ru>
2022-04-16 00:29:20 +0300

    add task 10

---
+++ seminars/task 10/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+int n = new Random().Next(100, 1000);
+
+int digit2 = (n / 10) % 10; 
+
+Console.WriteLine("Для числа N: ");
+Console.WriteLine(n);
+
+Console.WriteLine("Вторая цифра числа N: ");
+Console.Write(digit2);
