69f96444a92fab32703f0e4e9ccd7fec792f36ef
Jane <evgenya.mun@yandex.ru>
2022-04-16 00:18:31 +0300

    add changes

---
+++ seminars/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// первое число крато второму(делится без остатка). Если 1 не кратно 2, то вывод остаток от деления
+
+Console.Write("Введите первое число :");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число :");
+int b = Convert.ToInt32(Console.ReadLine());
+int div = a / b;
+
+if (a % b == 0)
+{
+    Console.WriteLine("Кратное");
+}
+else 
+{
+    Console.WriteLine("Не кратно, остаток " + a % b);
+}
