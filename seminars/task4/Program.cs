16c464f287107085d8d0029b371e6e32b82d9cd2
Jane <evgenya.mun@yandex.ru>
2022-04-16 00:39:16 +0300

    add changes

---
+++ seminars/task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число :");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int div = a / b;
+
+if (a % b == 0)
+{
+    Console.WriteLine("Кратное");
+}
+else 
+{
+    Console.WriteLine("Не кратно, остаток от деления = " + a % b);
+}
