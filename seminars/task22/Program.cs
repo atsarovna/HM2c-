0c3ff342578d3c9ee6249312214b3caf59851ec7
Jane <evgenya.mun@yandex.ru>
2022-04-19 23:14:32 +0300

    add task22

---
+++ seminars/task22/Program.cs
@@ -0,0 +1,26 @@
+﻿//Программа, которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+// 5 -> 1,4,9,16,25
+// 2 -> 1,4
+
+Console.Write("Enter number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+double result = 0;
+int index = 1;
+
+if (N <= 0)
+{
+    N=-N;
+}
+
+while(index<=N)
+{
+    result = Square (index);
+    Console.Write($"{result} ");
+    index++;
+}
+
+double Square (double number)
+{
+    return Math.Pow((number), 2);
+}
