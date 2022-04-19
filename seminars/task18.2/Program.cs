10c0f5cd88a2d006f7f7132b7f2e4a145cad0ce7
Jane <evgenya.mun@yandex.ru>
2022-04-19 20:25:10 +0300

    add task18.2

---
+++ seminars/task18.2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон 
+// возможных координат точек этой четверти (x и y) 
+
+// Чтобы выделить сразу весь текст - использовать /* и */ без // на каждой строке
+
+Console.Write("Enter quater from 1 to 4: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+if (quarter == 1)
+{
+    Console.WriteLine("range of coordinates for X from 0 to +infinity");
+    Console.WriteLine("range of coordinates for Y from 0 to +infinity");
+}
+if (quarter == 2)
+{
+    Console.WriteLine("range of coordinates for X from -infinity to 0");
+    Console.WriteLine("range of coordinates for Y from 0 to +infinity");
+}
+if (quarter == 3)
+{
+    Console.WriteLine("range of coordinates for X from -infinity to 0");
+    Console.WriteLine("range of coordinates for Y from -infinity to 0");
+}
+if (quarter == 4)
+{
+    Console.WriteLine("range of coordinates for X from 0 to +infinity");
+    Console.WriteLine("range of coordinates for Y from -infinity to 0");
+}
+if (quarter < 1 || quarter > 4)
+{
+    Console.WriteLine("Enter correct number of quarter");
+}
--- seminars/task18/Program.cs
+++ seminars/task18/Program.cs
@@ -1,32 +1,32 @@
 ﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон 
 // возможных координат точек этой четверти (x и y)
 
-//Решение через string (строку)
-//Console.Write("Enter quadrant: ");
-//int a = Convert.ToInt32(Console.ReadLine()); //a - номер четверти/квадранта
+/* Решение через string (строку)
+Console.Write("Enter quadrant: ");
+int a = Convert.ToInt32(Console.ReadLine()); //a - номер четверти/квадранта
 
-//string result = getCoordinates(a);
-//Console.WriteLine("This point is in coordinates: " + result);
+string result = getCoordinates(a);
+Console.WriteLine("This point is in coordinates: " + result);
 
-//if (a == 0 && a > 4) {
-//   Console.WriteLine("Enter quadrant from 1 to 4");
-//} else {
-//  Console.WriteLine("This point is in coordinates: " + c);
-//}
+if (a == 0 && a > 4) {
+   Console.WriteLine("Enter quadrant from 1 to 4");
+} else {
+  Console.WriteLine("This point is in coordinates: " + c);
+}
 
-//string getCoordinates(int a)
-//{
-//   if (a == 1)
-//       return "x > 0, y > 0";
-// if (a == 2)
-//       return "x < 0, y > 0";
-//  if (a == 3)
-//       return "x < 0, y < 0";
-//  if (a == 4)
-//      return "x > 0, y < 0";
+string getCoordinates(int a)
+{
+   if (a == 1)
+       return "x > 0, y > 0";
+ if (a == 2)
+       return "x < 0, y > 0";
+  if (a == 3)
+       return "x < 0, y < 0";
+  if (a == 4)
+      return "x > 0, y < 0";
 
-//  return "Enter quadrant from 1 to 4";
-//}
+  return "Enter quadrant from 1 to 4";
+} */
 
 // через void, но с int. Функция ничего не возвращает, поэтому return не нужен
 Console.Write("Enter quadrant: ");
