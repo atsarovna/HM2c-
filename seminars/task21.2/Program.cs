503b96ff53dfc06cb6f1c2173d88a169516df203
Jane <evgenya.mun@yandex.ru>
2022-04-20 08:50:43 +0300

    add task21

---
+++ seminars/task21.2/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
+// A(3,6); B(2,1) -> 5,09
+// A(7,-5); B(1,-1) -> 7,21
+
+//c помощью функции Math.Sqrt(double) - теорема Пифагора: квадрат гипотенузы равен сумме квадратов катета
+
+/*Console.Write("Enter coordinates X of first dot: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter coordinates Y of first dot: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter coordinates X of second dot: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter coordinates Y of second dot: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double a = (x2- x1)*(x2- x1);  //расстояние/размер 1 катета(нижняя сторона треугольника) - в квадрате или Math.Pow
+double b = (y2 - y1)*(y2 - y1); //расстояние/размер 2 катета(сторона справа)
+double c = a+b; // сумма квадратов катетов
+double distance = Gipotenusa(c); // объявить переменную типа double и она равна функции Гипотенуза,которая принимает аргумент с
+
+double Gipotenusa(double sum) // sum - сумма катетов
+{
+    return Math.Sqrt(sum); //возвращает корень из суммы катетов
+}
+Console.WriteLine($"Distance between 2 dots = {distance}");
+*/
+
+//второе решение
+/*Console.Write("Enter coordinates X of first dot: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter coordinates Y of first dot: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter coordinates X of second dot: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter coordinates Y of second dot: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+*/
+
+int x1 = coordinate("x","A");
+int y1 = coordinate("y","A");
+int x2 = coordinate("x","B");
+int y2 = coordinate("y","B");
+int coordinate(string coorName, string pointName)
+{
+    Console.Write($"Enter coordinates {coorName} of dot {pointName}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Coordinates are: A ({x1},{y1}) и B ({x2},{y2})");
+
+double katet1 = Math.Pow((x2-x1),2);
+double katet2 = Math.Pow((y2-y1),2);
+double gipotenusa = Math.Pow((katet1 + katet2),0.5); // Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow(y2-y1),2)
+
+Console.WriteLine($"Distance between 2 dots = {gipotenusa}");
--- seminars/task21/Program.cs
+++ seminars/task21/Program.cs
@@ -1,55 +1,26 @@
-﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
-// A(3,6); B(2,1) -> 5,09
-// A(7,-5); B(1,-1) -> 7,21
+﻿/* Задача 21: Напишите программу, которая принимает на
+вход координаты двух точек и находит расстояние между
+ними в 3D пространстве. */
+/* A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
 
-//c помощью функции Math.Sqrt(double) - теорема Пифагора: квадрат гипотенузы равен сумме квадратов катета
-
-/*Console.Write("Enter coordinates X of first dot: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter coordinates Y of first dot: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter coordinates X of second dot: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter coordinates Y of second dot: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-
-double a = (x2- x1)*(x2- x1);  //расстояние/размер 1 катета(нижняя сторона треугольника) - в квадрате или Math.Pow
-double b = (y2 - y1)*(y2 - y1); //расстояние/размер 2 катета(сторона справа)
-double c = a+b; // сумма квадратов катетов
-double distance = Gipotenusa(c); // объявить переменную типа double и она равна функции Гипотенуза,которая принимает аргумент с
-
-double Gipotenusa(double sum) // sum - сумма катетов
-{
-    return Math.Sqrt(sum); //возвращает корень из суммы катетов
-}
-Console.WriteLine($"Distance between 2 dots = {distance}");
-*/
-
-//второе решение
-/*Console.Write("Enter coordinates X of first dot: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter coordinates Y of first dot: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter coordinates X of second dot: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter coordinates Y of second dot: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-*/
-
-int x1 = coordinate("x","A");
+int x1 = coordinate("x","A"); 
 int y1 = coordinate("y","A");
+int c1 = coordinate("c","A");
 int x2 = coordinate("x","B");
 int y2 = coordinate("y","B");
+int c2 = coordinate("c","B");
 int coordinate(string coorName, string pointName)
 {
     Console.Write($"Enter coordinates {coorName} of dot {pointName}: ");
     return Convert.ToInt32(Console.ReadLine());
 }
 
-Console.WriteLine($"Coordinates are: A ({x1},{y1}) и B ({x2},{y2})");
+Console.WriteLine($"Coordinates are: A ({x1},{y1},{c1}) and B ({x2},{y2},{c2})");
 
-double katet1 = Math.Pow((x2-x1),2);
+double katet1 = Math.Pow((x2-x1),2); // возведение в квадрат
 double katet2 = Math.Pow((y2-y1),2);
-double gipotenusa = Math.Pow((katet1 + katet2),0.5); // Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow(y2-y1),2)
+double katet3 = Math.Pow((c2-c1),2);
+double gipotenusa = Math.Pow((katet1 + katet2 + katet3),0.5); // Math.Sqrt((katet1) + (katet2) + (katet3));
 
 Console.WriteLine($"Distance between 2 dots = {gipotenusa}");
