e5265303f6de909d0538080658bc8f4d54cc29fc
Jane <evgenya.mun@yandex.ru>
2022-04-22 15:42:03 +0300

    add task19 corrections

---
+++ seminars/task19/Program19.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да */
+
+Console.Write("Enter 5 digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+//проверка на отрицательные числа и на > 5 цифр в числе
+if (n < 99999 && n > 0)
+{
+    int dig1 = n / 10000;
+    int dig2 = (n / 1000) % 10;
+    int dig4 = (n / 10) % 10;
+    int dig5 = n % 10;
+    if ((dig1 == dig5) && (dig2 == dig4))
+        Console.WriteLine($"{n} -> Polindrom.");
+    else
+        Console.WriteLine($"{n} -> Not polindrom.");
+}
+else
+    Console.WriteLine("Incorrect number.Enter 5 digit number: ");
