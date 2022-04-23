9838355e62d374e9f1df35949943f27d5babe8b5
Jane <evgenya.mun@yandex.ru>
2022-04-23 19:47:16 +0300

    task27 corrections

--- seminars/task27/Program.cs
+++ seminars/task27/Program.cs
@@ -8,9 +8,9 @@ Console.Write("Enter the number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
 
-/* while(number > 0) */ for(int i = number;i > 0; i--)
+/* while(number > 0) */ for(int i = number;i > 0; i--) //считаем с конца
 {
-    sum = (number%10) + sum;
-    number = number / 10;
+    sum = (number%10) + sum; //находим последнюю цифру числа(остаток от деления) и складываем с найденными последними цифрами числа в порядке очереди
+    number = number / 10; //отбрасываем остаток,те избавляемся от последней цифры(целочисленное деления на 10)
 }
 Console.WriteLine("Sum of numbers is: " + sum);
