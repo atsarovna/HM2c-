f16c4cfc29f45ed175604be23ecb7132c3d548ed
Jane <evgenya.mun@yandex.ru>
2022-04-23 19:44:02 +0300

    task27 corrections

--- seminars/task27/Program.cs
+++ seminars/task27/Program.cs
@@ -1,4 +1,16 @@
-﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+﻿/* Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
 452 -> 11
 82 -> 10
 9012 -> 12 */
+
+Console.Write("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+/* while(number > 0) */ for(int i = number;i > 0; i--)
+{
+    sum = (number%10) + sum;
+    number = number / 10;
+}
+Console.WriteLine("Sum of numbers is: " + sum);
--- seminars/task28/Program.cs
+++ seminars/task28/Program.cs
@@ -5,5 +5,10 @@
 5 -> 120 */ // Факторил - 4 = 1*2*3*4 = 24
 
 Console.Write("Enter number: ");
-int fact = Convert.ToInt32(Console.ReadLine());
-
+int number = Convert.ToInt32(Console.ReadLine());
+int fact = 1;
+for (int i=1; i<=number; i++)
+{
+    fact = fact * i;
+}
+Console.WriteLine($"Factorial of the number is: {fact}");
