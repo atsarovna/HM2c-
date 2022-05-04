50bad304a5ec3ec3d73a2b078453c39831fa7dd4
Jane <evgenya.mun@yandex.ru>
2022-05-04 16:32:32 +0300

    task29

--- seminars/task29/Program29.cs
+++ seminars/task29/Program29.cs
@@ -6,16 +6,26 @@
 
 int[] array = new int[8];
 
+int min;
+
 Console.Write("Enter minimum number in array: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter maximum number in array: ");
-int max = Convert.ToInt32(Console.ReadLine());
-
-array [0] = min;
-array [array.Length - 1] = max;
-
-for (int i = 0; i<array.Length; i++)
+while (!int.TryParse(Console.ReadLine(), out min) || min < 0) //введенную строку записать в переменную до тех пор пока не успешно строка преобразована в число, я буду повторять попытку ввода до тех пор пока это не будет число и не будет положительным. проверка на буквы и отрицательные, если будет так,то будет повторять ввести верное число
 {
-    array[i] = new Random().Next(min,max);
-    Console.Write(array[i]+ " ");
+    Console.WriteLine("Wrong number, try again");
+}
+
+int max;
+
+Console.Write("Enter maximum number in array: ");
+while (!int.TryParse(Console.ReadLine(), out max) || max < 0) //введенную строку записать в переменную до тех пор пока не успешно строка преобразована в число, я буду повторять попытку ввода до тех пор пока это не будет число и не будет положительным. проверка на буквы и отрицательные, если будет так,то будет повторять ввести верное число
+{
+    Console.WriteLine("Wrong number, try again");
+}
+
+array[0] = min;
+array[array.Length - 1] = max;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(min, max);
+    Console.Write(array[i] + " ");
 }
