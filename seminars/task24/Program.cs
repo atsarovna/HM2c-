114a7f0461a0f03452a7dac24e805650b9a81588
Jane <evgenya.mun@yandex.ru>
2022-05-04 14:20:53 +0300

    task24 correctioты

--- seminars/task24/Program.cs
+++ seminars/task24/Program.cs
@@ -16,11 +16,11 @@ Console.WriteLine("Sum of numbers is: " + sum); */
 
 int number;
 
-Console.Write("Please enter positive ineger number: ");
+Console.Write("Please enter positive integer number: ");
 while (!int.TryParse(Console.ReadLine(), out number) || number < 0) //введенную строку записать в переменную до тех пор пока не успешно строка преобразована в число, я буду повторять попытку ввода до тех пор пока это не будет число и не будет положительным. проверка на буквы и отрицательные, если будет так,то будет повторять ввести верное число
 {
     Console.WriteLine("Wrong number, try again");
-    Console.Write("Please enter positive ineger number: ");
+    Console.Write("Please enter positive integer number: ");
 }
 
 int sum = 0;
