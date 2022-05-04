c9623ff9532f33327a670709601d0a07a96dfab9
Jane <evgenya.mun@yandex.ru>
2022-05-04 14:19:27 +0300

    task24 corrections,hw

--- seminars/task24/Program.cs
+++ seminars/task24/Program.cs
@@ -5,12 +5,27 @@
 4 -> 10
 8 -> 36 */
 
-Console.Write("Enter number: ");
+/* Console.Write("Enter number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
 for(int i = 1;i <= number; i++)
 {
     sum += i; // += - это sum + i
 }
-Console.WriteLine("Sum of numbers is: " + sum);
+Console.WriteLine("Sum of numbers is: " + sum); */
 
+int number;
+
+Console.Write("Please enter positive ineger number: ");
+while (!int.TryParse(Console.ReadLine(), out number) || number < 0) //введенную строку записать в переменную до тех пор пока не успешно строка преобразована в число, я буду повторять попытку ввода до тех пор пока это не будет число и не будет положительным. проверка на буквы и отрицательные, если будет так,то будет повторять ввести верное число
+{
+    Console.WriteLine("Wrong number, try again");
+    Console.Write("Please enter positive ineger number: ");
+}
+
+int sum = 0;
+for(int i = 1;i <= number; i++)
+{
+    sum += i;
+}
+Console.WriteLine("Sum of numbers is: " + sum); 
---
+++ seminars/task29/Program29.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 29: Напишите программу, которая задаёт 
+массив из 8 случайных целых чисел и выводит их на экран. 
+Диапазон - любое целое число.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int[] array = new int[8];
+
+Console.Write("Enter minimum number in array: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter maximum number in array: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+array [0] = min;
+array [array.Length - 1] = max;
+
+for (int i = 0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(min,max);
+    Console.Write(array[i]+ " ");
+}
