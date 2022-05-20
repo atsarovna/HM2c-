b363bb65d1871fb8e1584ec4b107fc1b6dd9790a
Jane <evgenya.mun@yandex.ru>
2022-05-20 15:27:05 +0300

    peremeshivanie

---
+++ seminars/перемешиваени/Program.cs
@@ -0,0 +1,19 @@
+﻿// перемешать цифры в массиве. пример рандомное распределение людей по группам
+// 1 алгоритм - перемешиваем рандомный индекс между собой
+// 2 алгоритм - создаем доп.массив со случайными числами и вовремя сортировки случайных чисел,сортируется первый массив, который тоже случайно распределяется
+
+//это 1 алгоритм, самый простой
+int[] numbers = { 1, 2, 3, 4, 5, 6};
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    int randomIndex = new Random().Next(0, numbers.Length);
+    int temp = numbers[i];
+    numbers[i] = numbers[randomIndex];
+    numbers[randomIndex] = temp;
+}
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
