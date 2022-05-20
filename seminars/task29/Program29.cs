5baa813bb7b93157b19b8668804b4504d2cda9bc
Jane <evgenya.mun@yandex.ru>
2022-05-20 14:52:54 +0300

    task29

--- seminars/task29/Program29.cs
+++ seminars/task29/Program29.cs
@@ -3,7 +3,7 @@
 Диапазон - любое целое число.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33] */
-
+/* 
 int[] array = new int[8];
 
 int min;
@@ -27,7 +27,7 @@ for (int i = 0; i < array.Length; i++)
     Console.Write(array[i] + " ");
 }
 
-for (int i = 0; i < array.Length; i++) 
+for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(0, 2);
     if (i == 0) //цикл проверки на последнюю цифру,чтобы не было в конце запятой
@@ -37,4 +37,50 @@ for (int i = 0; i < array.Length; i++)
     else
         Console.Write($"{array[i]}, ");
 }
-Console.WriteLine();
+Console.WriteLine(); */
+
+
+
+/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+/* Поправка: на семинаре дал добро на заволнение массива рандомными числами. 
+В оригинале нужно пользователю ввести строку - ряд чисел через запятую, а программа превращает эту строку в полноценный массив и выводит. 
+Рекомендую вторым вариантом) */
+
+string output = "3, 56, 78, 4, 5"; // пройтись циклом и ориентируясь на запятые выделить подстроки. подстрока с числом и с пробелом. 
+//int[] numbers = new int[];не знаем размер массива    //дальше преобразовать с помощью конверта в целые числа и засунуть в массив
+int countNumbers = 1; //с 1 потому что запятых 4, а цифр 5.
+for (int i = 0; i < output.Length; i++) //выяснить размер массива
+{
+    if (output[i] == ',')
+        countNumbers++; //получим кол-во чисел
+}
+
+int[] numbers = new int[countNumbers]; //создаем массив равный кол-ву запятых
+
+int numberIndex = 0;
+for (int i = 0; i < output.Length; i++)
+{
+    string subString = String.Empty; // переменную подстроку и дадим пустую строку
+
+    while (output[i] != ',') //записываем в строку символом столько пока не дойдет до запятой
+    {
+        if (i >= output.Length - 1)
+            break; //нужно выйти из цикла когда закончится массив
+
+        subString += output[i].ToString();
+        i++;
+    }
+
+    if (subString == "" || subString == " ")
+        continue; //если подстрока пустая и есть пробел
+
+    numbers[numberIndex] = Convert.ToInt32(subString);
+    numberIndex++;
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " "); //выводим массив
+}
---
+++ seminars/task46/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 46: Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1 */
+
+const int m = 4; //задаем кол-во столбцов. comst - потому что зафиксированные числа и меняться не будут
+const int n = 3; //кол-во строк
+
+int[,] arr = new int[m, n]; // создаю двумерный массив,выделяю память для массива с n строками и m колонками
+
+FillArray(arr, -100, 100); //задаем диапозон случайных чисел
+
+PrintArray(arr);
+
+//заполняю рандомными числами массив от мин до макс
+void FillArray(int[,] array, int min, int max)//в скобках аргумент. int[,]- двумерный массив, array - название массива, int min, int max - диапозон
+{
+    Random rnd = new Random();//генератор случайных чисел
+
+    for (int i = 0; i < array.GetLength(0); i++) //array.GetLength(0) - функция с аргументом номер размерностию 0 - размерность,первое измерение это будет m
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max);
+}
+
+//вывожу массив в консоль
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.WriteLine($"array[{i},{j}] = {array[i, j]}");
+}
