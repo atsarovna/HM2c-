9bfa2023f0b5601fc9ace2463ecdb982b4d1c09f
Jane <evgenya.mun@yandex.ru>
2022-05-21 20:25:06 +0300

    task48

--- seminars/task48/Program.cs
+++ seminars/task48/Program.cs
@@ -6,4 +6,40 @@ m = 3, n = 4.
 1 2 3 4
 2 3 4 5 */
 
+int m = InputValue("lines"); //строки
+int n = InputValue("columns"); //столбцы
 
+int[,] array = new int[m, n];
+
+FillArray(array);
+
+PrintArray(array);
+
+int InputValue(string n)
+{
+    int value;
+    Console.Write($"Enter count of {n}: ");
+    while (!int.TryParse(Console.ReadLine(), out value))
+    {
+        Console.WriteLine("Error. Try again.");
+        Console.Write($"Enter count of {n}: ");
+    }
+    return value;
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = i + j;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+    Console.WriteLine();
+    }
+}
